539c902b38a9815c97319407b11bb66d2de0d9cc
Szabó Gábor <szabogabi@gmail.com>
2014-05-07 18:14:44 +0200

    javitasok kekites

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '281a6498ad2f89bbbc726c185ee3f419');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f4f0361cc1374fa83a23416d3e612e24');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
