0c3ec62e1e557ceccd44d25cd2d7a328c37bab5c
Szabó Gábor <szabogabi@gmail.com>
2014-05-13 18:51:10 +0200

    colors delete

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '5efb642635d06d091c0b30e2385e2438');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ab922dce81bc2002ae6da151dbf95171');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
