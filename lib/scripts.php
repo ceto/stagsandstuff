2a27431a278d881dcddd9094034b56a72ade0205
Szabó Gábor <szabogabi@gmail.com>
2014-07-14 13:47:17 +0200

    footer jav

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '24e88894df1be3e4f7cdd0e1cd6515c2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c5f27a51e31c3216c065dddfff52f1e6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
