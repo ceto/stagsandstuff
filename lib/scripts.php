bff8ddca6cc06e1d8348ffc142c9b12f5cc4d9a5
Szabó Gábor <szabogabi@gmail.com>
2014-07-31 13:22:51 +0200

    activity subtitle c color

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c77ec9e16e3c566c22a9cadc59f559ad');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '8a73b5d7dbecc94b8a99dcd6a8313b7d');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
