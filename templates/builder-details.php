3bcd3f41fd28332b27d067377d0a6b683221f073
Szabó Gábor <szabogabi@gmail.com>
2014-08-11 12:43:37 +0200

    bg csere es contact form mod

--- templates/builder-details.php
+++ templates/builder-details.php
@@ -1,6 +1,7 @@
 <div class="details-wrap">
     <div id="startpanel" class="brokielso">
       <h2 class="ctitle">Enquire Custom Package</h2>
+    </div>
 
 
       <div id="firstpanel" class="broki">
@@ -39,35 +40,37 @@
 
 
     </div><!-- /.tab-pane -->
+
    
+   <div id="thirdpanel" class="broki">
 
-         <h3 class="csubtitle">Add your details</h3>
-               <hr>
+        <h3 class="csubtitle">Add your details</h3>
+        <hr>
 
-    <div class="leftfields">
-      <div class="items">
-        <label for="message_name">Name *</label>
-        <input type="text" required placeholder="John Doe" id="message_name" name="message_name" value="<?php echo $_POST['message_name']; ?>">
-      </div>
-      <div class="items">
-        <label for="message_name">E-mail *</label>
-        <input type="email" required placeholder="name@example.com" id="message_name" name="message_name" value="<?php echo $_POST['message_name']; ?>">
-      </div>
-      <div class="items">
-        <label for="message_tel">Phone</label>
-        <input type="tel" placeholder="003612345678" id="message_tel" name="message_tel" value="<?php echo $_POST['message_tel']; ?>">
-      </div>
-    </div>
-    <div class="rightfields">
-      <div class="items">
-        <label for="message_date">Date *</label>
-        <input type="date" required id="message_date" name="message_date" value="<?php echo $_POST['message_date']; ?>">
-      </div>
-      <div class="items">
-          <label for="message_text">Message</label>
-          <textarea placeholder="Add any question here ..." rows="4" id="message_text" name="message_text"><?php echo $_POST['message_text']; ?></textarea>
-      </div>
-    </div>
+        <div class="leftfields">
+          <div class="items">
+            <label for="message_name">Name *</label>
+            <input type="text" required placeholder="John Doe" id="message_name" name="message_name" value="<?php echo $_POST['message_name']; ?>">
+          </div>
+          <div class="items">
+            <label for="message_name">E-mail *</label>
+            <input type="email" required placeholder="name@example.com" id="message_name" name="message_name" value="<?php echo $_POST['message_name']; ?>">
+          </div>
+          <div class="items">
+            <label for="message_tel">Phone</label>
+            <input type="tel" placeholder="003612345678" id="message_tel" name="message_tel" value="<?php echo $_POST['message_tel']; ?>">
+          </div>
+        </div>
+        <div class="rightfields">
+          <div class="items">
+            <label for="message_date">Date *</label>
+            <input type="date" required id="message_date" name="message_date" value="<?php echo $_POST['message_date']; ?>">
+          </div>
+          <div class="items">
+              <label for="message_text">Message</label>
+              <textarea placeholder="Add any question here ..." rows="4" id="message_text" name="message_text"><?php echo $_POST['message_text']; ?></textarea>
+          </div>
+        </div>
 
     </div>
 
